cb57bbb137e656a4e644f158ecd431dadafcc0f2
Richlyn <richlyn.tamm@gmail.com>
2019-03-15 08:06:36 +0100

    refactored filters

--- scripts/main.js
+++ scripts/main.js
@@ -60,85 +60,10 @@ function fullName(last_name, middle_name, first_name) {
     : last_name + ", " + middle_name + " " + first_name;
 }
 
-/*function memberTable(allMembers) {
-  var tBody = document.getElementById("tBody");
-  var party = document.querySelectorAll('input[type="checkbox"]:checked');
-  var partyArray = Array.from(party);
-
-  if (partyArray.length > 0) {
-    tBody.innerHTML = "";
-    tBody.insertRow("tRow");
-    for (var i = 0; i < allMembers.length; i++) {
-      for (var j = 0; j < partyArray.length; j++) {
-        if (allMembers[i].party == partyArray[j].value) {
-          //comparing arrays and not indexes not creating a false positive
-          var tRow = tBody.insertRow();
-          tRow.insertCell().innerHTML = i + 1 + ".";
-
-          if (allMembers[i].middle_name == null) {
-            var repName =
-              allMembers[i].last_name + ", " + allMembers[i].first_name;
-            tRow.insertCell().innerHTML = repName.link(allMembers[i].url);
-          } else {
-            var repName =
-              allMembers[i].last_name +
-              ", " +
-              allMembers[i].first_name +
-              " " +
-              allMembers[i].middle_name;
-            tRow.insertCell().innerHTML = repName.link(allMembers[i].url);
-          }
-          tRow.insertCell().innerHTML = allMembers[i].party;
-          tRow.insertCell().innerHTML = allMembers[i].state;
-
-          if (allMembers[i].seniority == 1) {
-            tRow.insertCell().innerHTML = allMembers[i].seniority + " year";
-          } else {
-            tRow.insertCell().innerHTML = allMembers[i].seniority + " years";
-          }
-          tRow.insertCell().innerHTML =
-            allMembers[i].votes_with_party_pct + "%";
-          tBody.append(tRow);
-        }
-      }
-    }
-  } else {
-    tBody.innerHTML = "";
-    for (var i = 0; i < allMembers.length; i++) {
-      var tRow = tBody.insertRow();
-      tRow.insertCell().innerHTML = i + 1 + ".";
-
-      if (allMembers[i].middle_name == null) {
-        var repName = allMembers[i].last_name + ", " + allMembers[i].first_name;
-        tRow.insertCell().innerHTML = repName.link(allMembers[i].url);
-      } else {
-        var repName =
-          allMembers[i].last_name +
-          ", " +
-          allMembers[i].first_name +
-          " " +
-          allMembers[i].middle_name;
-        tRow.insertCell().innerHTML = repName.link(allMembers[i].url);
-      }
-      tRow.insertCell().innerHTML = allMembers[i].party;
-      tRow.insertCell().innerHTML = allMembers[i].state;
-
-      if (allMembers[i].seniority == 1) {
-        tRow.insertCell().innerHTML = allMembers[i].seniority + " year";
-      } else {
-        tRow.insertCell().innerHTML = allMembers[i].seniority + " years";
-      }
-
-      tRow.insertCell().innerHTML = allMembers[i].votes_with_party_pct + "%";
-    }
-  }
-}
-// memberTable(allMembers); */
-
-for (var i = 0; i < usStates.length; i++) {
+for (var s = 0; s < usStates.length; s++) {
   var option = document.createElement("option");
-  option.text = usStates[i].name;
-  option.value = usStates[i].abbreviation;
+  option.text = usStates[s].name;
+  option.value = usStates[s].abbreviation;
   var select = document.getElementById("state");
 
   select.appendChild(option);
